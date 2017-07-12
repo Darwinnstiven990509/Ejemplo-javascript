d26d21a6e91c5596de873879295aaeba3a5c1129
Darwinn stiven <darwincastro562@gmail.com>
2017-07-12 09:51:42 -0500

    ejemplo de javascript con alert y confirm

---
+++ ejemplo.js
@@ -0,0 +1,9 @@
+alert("hola mundo")
+if (confirm("estas contento con la clase")){
+	// lo que se hace si se cumple la funcion
+	alert("me caes bien")
+} else {
+	// lo que se hace sino se cumple la funcion 
+	alert("mejor quedese durmiendo en la casa");
+}
+
